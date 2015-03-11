0efe860c5522982ea603673ae3282d1ae66ea273
VaneChipi <vchipirras@hotmail.com>
2015-03-11 22:03:52 +0100

    Arreglado el error del ejercicio 3 en el fichero prog.c

--- ejercicios/3/3/prog.c
+++ ejercicios/3/3/prog.c
@@ -1,11 +1,13 @@
 /* Programa para leer mensajes del terminal y escribirlos */
+/* No tenia memoria suficiente para poder guardar la cadena en el buffer, porque tenia definido poca memoria. Podría haber puesto 50 de tamaño pero quise 
+poner el tamaño exacto */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **argv)
 {
-	char buf[5];
+	char buf[33];
 
 	snprintf(buf, 50, "Que tipo de error de memoria soy");
 	printf("%s\n", buf);
